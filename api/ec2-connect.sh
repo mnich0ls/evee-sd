ec07e0763b8f2e664c4d4c99b6c68e93d27e359c
jremi <jzbgdev@gmail.com>
2019-04-22 10:49:42 -0700

    Updated EC2 instance for connect script

--- api/ec2-connect.sh
+++ api/ec2-connect.sh
@@ -1,1 +1,1 @@
-ssh -i ./private-key/aws_ec2-evee-api.pem ubuntu@ec2-13-52-178-95.us-west-1.compute.amazonaws.com
+ssh -i ./private-key/evee-events-api.pem ubuntu@ec2-13-52-186-249.us-west-1.compute.amazonaws.com
