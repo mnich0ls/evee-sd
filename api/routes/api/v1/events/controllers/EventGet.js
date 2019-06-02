94c5731a5f16235f90375111881a79a8233f73ba
Mike Nichols <mnich0ls@icloud.com>
2019-06-02 16:53:19 -0700

    Update GET Events API
    Add filter by locations, price, and date

--- api/routes/api/v1/events/controllers/EventGet.js
+++ api/routes/api/v1/events/controllers/EventGet.js
@@ -14,8 +14,8 @@ module.exports = function(values, callback){
     var limit = 50;
 
     // Perform a start_date ISO8601 (YYYY-MM-DD) validation
-    if(values.start_date){
-        if(!validations.isDate_ISO8601(values.start_date)){
+    if(values.date){
+        if(!validations.isDate_ISO8601(values.date)){
             callback({
                 message: 'You have invalid values provided for parameters.',
                 error_code: 'INVALID_PARAM_VALUES',
@@ -25,31 +25,59 @@ module.exports = function(values, callback){
         }
     }
 
+    // start with any join queries 
+    // we have a lookup table to map categories to our own categories 
+
+
     // Check if the values from GET request contain start_date and/or category
     if(filters.includes('categories')){
         // Setup a query to filter events only by category
         whiteListedValues.push((values['categories']))
         SQL += " JOIN category_map m ON m.original_category = e.category AND m.mapped_category in (?)"
     }
-
     SQL += " WHERE e.status = 'active'"
-    // todo - refactor the start_date param - 
-    // the goal is to search for events on or after a certain date (which means we need to search the end_date event property)
+
+    if (filters.includes('search')) {
+        whiteListedValues.push('%' + values['search'] + '%')
+        SQL += " AND e.title like ?"
+    }
+
+    if (filters.includes('price')) {
+        let price = values['price'];
+        if (price === 'free') {
+            SQL += " AND e.price = 0"
+        } else if (price === 'paid') {
+            // a price less than 0 means the price can vary 
+            SQL += " AND e.price != 0"
+        }
+    }
+
+    if (filters.includes('locations')) {
+        let locations = values['locations']
+        if (Array.isArray(locations)) {
+            console.log('Search by array location', locations)
+            whiteListedValues.push(locations.shift() + '%')
+            SQL += " AND (e.location like ?"
+            locations.forEach((l) => {
+                whiteListedValues.push(l + '%')
+                SQL += " OR e.location like ?"
+            })
+            SQL += ")"
+        } else {
+            console.log('Search by string location', locations)
+            whiteListedValues.push(locations + '%')
+            SQL += " AND e.location like ?"
+        }
+    }
+
     let today = moment().format('YYYY-MM-DD')
-    if(filters.includes('start_date') && moment(today).isBefore(moment(values['start_date']))){
-        // Setup a query to filter events only by start_date
-        // only if the start date is later than today 
-        whiteListedValues.push(values['start_date'])
+    if(filters.includes('date') && moment(today).isBefore(moment(values['date']))){
+        whiteListedValues.push(values['date'])
         SQL += " AND e.end_date >= ?"
     } else {
         // default to events ending today or later
         whiteListedValues.push(today)
         SQL += " AND e.end_date >= ?"
-    }
-
-    if (filters.includes('search')) {
-        whiteListedValues.push('%' + values['search'] + '%')
-        SQL += " AND e.title like ?"
     }
 
     SQL += " ORDER BY e.start_date LIMIT ? OFFSET ?";
