51dc35969867a7a623ae3dfb79950ed5192bc4b9
Mike Nichols <mnich0ls@icloud.com>
2019-06-02 15:31:25 -0700

    Update GET Events API
    Added filter by category

--- api/routes/api/v1/events/controllers/EventGet.js
+++ api/routes/api/v1/events/controllers/EventGet.js
@@ -10,7 +10,7 @@ module.exports = function(values, callback){
 
     var filters = Object.keys(values);
     var whiteListedValues = []
-    var SQL = "SELECT * FROM events WHERE status = 'active'";
+    var SQL = "SELECT e.* FROM events e";
     var limit = 50;
 
     // Perform a start_date ISO8601 (YYYY-MM-DD) validation
@@ -26,12 +26,13 @@ module.exports = function(values, callback){
     }
 
     // Check if the values from GET request contain start_date and/or category
-    if(filters.includes('category')){
+    if(filters.includes('categories')){
         // Setup a query to filter events only by category
-        whiteListedValues.push((values['category']).toLowerCase())
-        SQL += " AND category = ?"
+        whiteListedValues.push((values['categories']))
+        SQL += " JOIN category_map m ON m.original_category = e.category AND m.mapped_category in (?)"
     }
 
+    SQL += " WHERE e.status = 'active'"
     // todo - refactor the start_date param - 
     // the goal is to search for events on or after a certain date (which means we need to search the end_date event property)
     let today = moment().format('YYYY-MM-DD')
@@ -39,19 +40,19 @@ module.exports = function(values, callback){
         // Setup a query to filter events only by start_date
         // only if the start date is later than today 
         whiteListedValues.push(values['start_date'])
-        SQL += " AND end_date >= ?"
+        SQL += " AND e.end_date >= ?"
     } else {
         // default to events ending today or later
         whiteListedValues.push(today)
-        SQL += " AND end_date >= ?"
+        SQL += " AND e.end_date >= ?"
     }
 
     if (filters.includes('search')) {
         whiteListedValues.push('%' + values['search'] + '%')
-        SQL += " AND title like ?"
+        SQL += " AND e.title like ?"
     }
 
-    SQL += " ORDER BY start_date LIMIT ? OFFSET ?";
+    SQL += " ORDER BY e.start_date LIMIT ? OFFSET ?";
     whiteListedValues.push(limit);
 
     if(!filters.includes('page')) {
